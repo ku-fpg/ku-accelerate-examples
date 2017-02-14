5b18434c5fb237df4baa6bfa3069be4b01d0bf4c
David Young <roboguy06@gmail.com>
2017-02-14 14:05:24 -0600

    More consistency between examples

--- fib/FibSouth.hs
+++ fib/FibSouth.hs
@@ -10,9 +10,9 @@ fib n = runTailRec go (0, 1, n)
       -- go :: (Int, Int, Int) -> If (Int, Int, Int) (Iter (Int, Int, Int) Int)
       go :: TailRec (Int, Int, Int) Int
       go (a, b, i)
-        = If (\(a, b, i) -> i == 0)
-             (done b)
+        = If (\(a, b, i) -> i > 0)
              (step (b, (a+b), (i-1)))
+             (done b)
 
 fibSpec :: Int -> Int
 fibSpec 0 = 1
