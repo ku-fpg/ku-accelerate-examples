ace37002d9f2b01b646abb821b6d0ebc3d333c94
David Young <roboguy06@gmail.com>
2017-03-01 15:18:27 -0600

    More work on unifying the types into one

--- fib/South.hs
+++ fib/South.hs
@@ -1,29 +1,33 @@
 -- | Combines the elements of the southbank language into one data type
 
 {-# LANGUAGE GADTs, KindSignatures, DeriveFunctor, StandaloneDeriving #-}
-
+{-# LANGUAGE ConstraintKinds, PolyKinds, TypeOperators #-}
 module South where
 
 import           Iter
 import           TailRec
 import           If
 
+import           Data.Proxy
+import           Data.Type.Equality
+
 data South :: * -> * -> * where
-  IfS   :: If   a b -> Maybe (South x b) -> South a b
-  IterS :: Iter a b -> Maybe (South x b) -> South a b
+  IfS   :: (a -> If   a b) -> (b -> South b c) -> South a c
+  IterS :: (a -> Iter a b) -> (b -> South b c) -> South a c
+  FinishS :: b -> South a b
 
 deriving instance Functor (South a)
 
 stepS :: b -> South b a
-stepS = (`IterS` Nothing) . step
+stepS x = IterS (const (step x)) FinishS
 
 doneS :: a -> South b a
-doneS = (`IterS` Nothing) . done
+doneS x = IterS (const (done x)) FinishS
 
 -- XXX: Does this make sense with the type as it is now?
 runSouth :: (a -> South a b) -> a -> b
 runSouth f x =
   case f x of
-    IfS   i k -> runIf    (const i) x
-    IterS i k -> iterLoop (const i) x
+    IfS   i k -> runSouth k (runIf    i x)
+    IterS i k -> runSouth k (iterLoop i x)
 
